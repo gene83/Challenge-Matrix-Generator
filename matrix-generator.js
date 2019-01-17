a327aba70503468965667f2424274563e14ec787
Gene83 <gene83@protonmail.com>
2019-01-17 09:30:48 -1000

    use compare function for normal tests and insertion sort for stretch goals

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,24 @@
 function matrixGenerator(rows, columns) {
-  // do work here
+  const matrix = [];
+
+  for (let y = 0; y < rows; y++) {
+    matrix[y] = [];
+    for (let x = 0; x < columns; x++) {
+      matrix[y][x] = Math.round(Math.random() * 100);
+    }
+  }
+
+  matrix.forEach(row => row.sort((a, b) => a - b));
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (matrix[i][0] < matrix[j][0]) {
+        const temp = matrix.splice(i, 1);
+        matrix.splice(j, 0, temp[0]);
+      }
+    }
+  }
+  return matrix;
 }
 
 module.exports = matrixGenerator;
